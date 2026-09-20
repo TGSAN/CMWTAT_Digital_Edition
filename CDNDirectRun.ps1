db2ff03fbf6e7a19d08f288c89729497d519da48
TGSAN <h@iloli.tv>
2026-09-20 17:33:36 +0800

    Update CDNDirectRun.ps1

--- CDNDirectRun.ps1
+++ CDNDirectRun.ps1
@@ -12,4 +12,9 @@ if (-not $isAdmin) {
 $bytes = (Invoke-WebRequest "https://release-download.static.cloudmoe.com/TGSAN/CMWTAT_Digital_Edition/releases/download/3.0.1.0/CMWTAT_Digital_Release_3_0_1_0.exe").Content
 $assembly = [System.Reflection.Assembly]::Load($bytes)
 $entryPointMethod = $assembly.EntryPoint
-$entryPointMethod.Invoke($null, @())
+$params = $entryPointMethod.GetParameters()
+if ($params.Count -eq 0) {
+    $entryPointMethod.Invoke($null, @())
+} else {
+    $entryPointMethod.Invoke($null, @(, [string[]]@()))
+}
